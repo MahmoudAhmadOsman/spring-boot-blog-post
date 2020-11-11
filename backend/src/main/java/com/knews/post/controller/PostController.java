97d64ced7e169f4e1d3f194811b2433ff6cd3aef
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-11-11 16:07:18 -0600

    added delete method

--- backend/src/main/java/com/knews/post/controller/PostController.java
+++ backend/src/main/java/com/knews/post/controller/PostController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -16,6 +18,8 @@ public class PostController {
     @Autowired
     private PostRepository postRepository;
 
+
+    //@GET ALL POSTS METHOD
     @GetMapping("/posts")
     public List<Post> getAllPosts(){
         return postRepository.findAll();
@@ -28,7 +32,7 @@ public class PostController {
         return postRepository.save(post);
 
     }
-
+    //@GET BY IF METHOD
     @GetMapping("/posts/{id}")
     public ResponseEntity<Post> getPostById(@PathVariable Long id){
         Post post = postRepository.findById(id)
@@ -36,11 +40,44 @@ public class PostController {
         return  ResponseEntity.ok(post);
     }
 
+    //@EDIT OR UPDATE METHOD
 
 
+    //@DELETE METHOD
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Map<String, Boolean>> deletePost(@PathVariable Long id){
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Posts with this id does not exist :" + id));
+
+        postRepository.delete(post);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Post has been deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 
 
-
-
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
