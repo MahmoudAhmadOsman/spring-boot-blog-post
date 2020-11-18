07dd1a3dc455e9ee73fdb6eb5027d8712941ad78
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-11-18 02:34:05 -0600

    added nav, post-list & footer components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
